d0e750da6aef6dd8ab7146e984506bcbb6955084
Stéphane SCHMIDELY <s.schmidely@gmail.com>
2021-07-03 17:04:46 +0200

    refactor(models): update command to support money given

--- coffeemachine/src/app/models/command.ts
+++ coffeemachine/src/app/models/command.ts
@@ -5,24 +5,25 @@ type SugarQuantity = 0 | 1 | 2;
 interface Command {
   drink: DrinkTypes;
   sugarQuantity: SugarQuantity;
+  moneyGiven: number;
 }
 
 class TeaCommand implements Command {
   drink = new Tea();
 
-  constructor(public sugarQuantity: SugarQuantity = 0) {}
+  constructor(public moneyGiven: number = 0, public sugarQuantity: SugarQuantity = 0) {}
 }
 
 class CoffeeCommand implements Command {
   drink = new Coffee();
 
-  constructor(public sugarQuantity: SugarQuantity = 0) {}
+  constructor(public moneyGiven: number = 0, public sugarQuantity: SugarQuantity = 0) {}
 }
 
 class ChocolateCommand implements Command {
   drink = new Chocolate();
 
-  constructor(public sugarQuantity: SugarQuantity = 0) {}
+  constructor(public moneyGiven: number = 0, public sugarQuantity: SugarQuantity = 0) {}
 }
 
 type DrinkCommand = TeaCommand | CoffeeCommand | ChocolateCommand;
