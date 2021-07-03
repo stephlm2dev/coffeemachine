ae03ab46542283bdeb7e19861ddc98e675df355b
Stéphane SCHMIDELY <s.schmidely@gmail.com>
2021-07-03 17:04:28 +0200

    tests(services): update drink maker to support money

--- coffeemachine/src/app/drink-maker.service.spec.ts
+++ coffeemachine/src/app/drink-maker.service.spec.ts
@@ -17,21 +17,39 @@ describe('DrinkMakerService', () => {
     expect(service).toBeTruthy();
   });
 
+  // OK coffee maker tests
   it('should make 1 tea with 1 sugar and a stick', () => {
-    const tea = new TeaCommand(1);
+    const tea = new TeaCommand(1, 1);
     expect(service.prepareCommand(tea)).toBe("T:1:0");
   });
 
-  it('should makes 1 chocolate with no sugar - and therefore no stick', () => {
-    const chocolate = new ChocolateCommand();
+  it('should make 1 chocolate with no sugar - and therefore no stick', () => {
+    const chocolate = new ChocolateCommand(2);
     expect(service.prepareCommand(chocolate)).toBe("H::");
   });
 
-  it('should makes 1 coffee with 2 sugars and a stick', () => {
-    const coffee = new CoffeeCommand(2);
+  it('should make 1 coffee with 2 sugars and a stick', () => {
+    const coffee = new CoffeeCommand(2, 2);
     expect(service.prepareCommand(coffee)).toBe("C:2:0");
   });
 
+  // KO Not enough money given tests
+  it('should NOT make 1 tea with 1 sugar and a stick', () => {
+    const tea = new TeaCommand(0, 1);
+    expect(service.prepareCommand(tea)).toBe("M:0.4€ are missing");
+  });
+
+  it('should NOT make 1 chocolate with no sugar - and therefore no stick', () => {
+    const chocolate = new ChocolateCommand(0);
+    expect(service.prepareCommand(chocolate)).toBe("M:0.5€ are missing");
+  });
+
+  it('should NOT make 1 coffee with 2 sugars and a stick', () => {
+    const coffee = new CoffeeCommand(0, 2);
+    expect(service.prepareCommand(coffee)).toBe("M:0.6€ are missing");
+  });
+
+  // Message tests
   it('should forwards any message received onto the coffee machine interface', () => {
     expect(service.prepareCommand({
       name: MessageTypes.DEFAULT,
