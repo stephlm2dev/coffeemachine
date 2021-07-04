18032e3fa29dc70196f83b6eee94a56a2339298e
Stéphane SCHMIDELY <s.schmidely@gmail.com>
2021-07-04 18:16:54 +0200

    refactor(services): cleaner code

--- coffeemachine/src/app/drink-maker.service.ts
+++ coffeemachine/src/app/drink-maker.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 
-import { DrinkCommand } from './models/command';
+import DrinkCommand from './models/command';
 import { MessageCommand, MessageTypes } from './models/message';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DrinkMakerService {
+
   constructor() {}
 
   prepareCommand(command: DrinkCommand | MessageCommand) {
     let cmd = null;
 
-    if ('sugarQuantity' in command) {
-      cmd = this.canPayDrink(command)
+    if (this.isDrinkCommand(command)) {
+      cmd = this.canPayDrink(command as DrinkCommand)
         ? this.makeDrinkCommand(command as DrinkCommand)
         : this.missingMoney(command as DrinkCommand);
     } else {
@@ -33,15 +34,19 @@ export class DrinkMakerService {
     return `${message.name}:${message.message}`;
   }
 
+  private isDrinkCommand(command: DrinkCommand | MessageCommand) {
+    return command instanceof DrinkCommand
+  }
+
   private canPayDrink(command: DrinkCommand) {
-    return command.drink.price <= command.moneyGiven;
+    return command.drink.price <= command.money;
   }
 
   private missingMoney(command: DrinkCommand) {
     const message: MessageCommand = {
       name: MessageTypes.DEFAULT,
       message: `${Math.abs(
-        command.drink.price - command.moneyGiven
+        command.drink.price - command.money
       )}€ are missing`,
     };
     return this.prepareMessageCommand(message);
